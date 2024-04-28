30b66793015610d689cb90d2b10a0036880dc8f2
suraj-jha01 <167965906+suraj-jha01@users.noreply.github.com>
2024-04-29 00:02:19 +0530

    Delete Generate a set containing  random numbers in the range.py

--- Generate a set containing  random numbers in the range.py
+++
@@ -1,7 +0,0 @@
-import random
-random_numbers_set = set()
-while len(random_numbers_set) < 10:
-    random_number = random.randint(15, 45)
-    random_numbers_set.add(random_number)
-
-print("Original set of random numbers:", random_numbers_set)
